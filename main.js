213f044d4c4365d9e4c0a4e2b092bde2ae70462e
lukas <lukaswastaken@icloud.com>
2024-07-07 21:47:59 +0200

    Changed by Lukas: - Added /test route which responses with "Server's fine" - Added /register/:username route for testing

--- main.js
+++ main.js
@@ -8,6 +8,17 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'view', 'index.html'));
 });
 
+//Req = request -> enthält infos vom client
+//Res = response -> enthält infos die der Server wegschickt
+app.get('/test', (req, res) => {
+    res.send('Server\'s fine')
+})
+
+app.get('/register/:username', (req, res) => {
+    let username = req.params.username
+    console.log(username)
+    res.send(username)
+})
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
