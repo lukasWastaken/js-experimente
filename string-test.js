ecff905e7d3dae79374f7fa360e52d99b15de4f3
lukas <lukaswastaken@icloud.com>
2024-07-12 11:00:08 +0200

    added a string manipulation file

---
+++ string-test.js
@@ -0,0 +1,13 @@
+let text = "Hallo Welt Test";
+
+// Splitting output
+const new_text = text.split(" ")
+console.log(new_text)
+
+//Upper case output
+var uppercase = text.toUpperCase();
+console.log(uppercase)
+
+//Lower case output
+const lowercase = uppercase.toLowerCase();
+console.log(lowercase)
